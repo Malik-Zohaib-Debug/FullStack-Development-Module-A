d494c41c04097aeed73a5b7886c836ad536fd680
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2022-02-24 19:55:24 +0500

    added new features
    added new features in application

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -26,15 +26,51 @@ function addData(newUser) {
     updateDOM();
 }
 
-function updateDOM(userData = data){
+function updateDOM(userData = data) {
     main.innerHTML = `<h2><strong>User</strong> Wealth</h2>`;
     userData.forEach(user => {
-        const userDiv = document.createElement('div');        
+        const userDiv = document.createElement('div');
         userDiv.classList.add('user');
-        userDiv.innerHTML = `<strong>${user.name}</strong>${user.balance}`
+        userDiv.innerHTML = `<strong>${user.name}</strong>
+                            ${formatNumberToDollar(user.balance)}`;
         main.appendChild(userDiv);
     });
 }
+
+function doubleMoney(){
+    data = data.map(user => {
+        return {...user, balance: user.balance * 2}
+    })
+    updateDOM();
+}
+
+function filterUser(){
+    data = data.filter(user => user.balance >= 1000000);
+    updateDOM();
+}
+
+function sortBalance(){
+    data = data.sort((a,b) => b.balance - a.balance)
+    updateDOM
+}
+
+function formatNumberToDollar(number){
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+function totalBalance(){
+    updateDOM();
+    const balance = data.reduce((acc, user) => (acc += user.balance), 0);
+    const balanceElement = document.createElement('div');
+    balanceElement.innerHTML = `<h3>Total Balance: ${formatNumberToDollar(balance)}</h3>`;
+    main.appendChild(balanceElement);
+}
+
+doubleBtn.addEventListener('click', doubleMoney);
+filterBtn.addEventListener('click', filterUser);
+addUserBtn.addEventListener('click', getRandomUser);
+sortBtn.addEventListener('click', sortBalance);
+sumBtn.addEventListener('click', totalBalance);
 
 getRandomUser()
 getRandomUser()
